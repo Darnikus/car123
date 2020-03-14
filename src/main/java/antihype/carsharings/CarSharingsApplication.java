b6be42a4e5326285c9e28f85b60091a00da820e8
Darnikus <SuperDarnikus@gmail.com>
2020-03-14 19:27:32 +0200

    first commit

---
+++ src/main/java/antihype/carsharings/CarSharingsApplication.java
@@ -0,0 +1,13 @@
+package antihype.carsharings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarSharingsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarSharingsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/antihype/carsharings/CarSharingsApplicationTests.java
@@ -0,0 +1,13 @@
+package antihype.carsharings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarSharingsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
